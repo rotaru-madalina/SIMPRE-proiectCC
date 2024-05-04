df912571268edb90d47d39aae8867434a5bb405f
rotaru-madalina <r.madalina0@gmail.com>
2024-05-04 19:05:29 +0300

    updated csr.jsx

--- pages/csr.jsx
+++ pages/csr.jsx
@@ -1,28 +1,25 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState} from 'react'
 
-const csr = () => {
-    const [data, setData] = useState({});
+const Csr = () => {
+  const [data, setData] = useState({});
 
-    const getUser = async () => {
-        const response = await fetch('https://randomuser.me/api/?nat=us&randomapi');
+  const getUser = async () => {
+    const response = await fetch('https://randomuser.me/api/?nat=us&randomapi');
 
-        const data = await response.json();
+    const data = await response.json();
 
-        //console.log(data);
-        setData(data.results[0]);
-    }
+    setData(data.results[0]);
+  }
 
-    useEffect(() => {
-        getUser();
-    }, []);
+  useEffect(() => {
+    getUser();
+  }, []);
 
-    //console.log(data)
-
-    return (
-        <div>
-            Hello, {data?.name?.title} {data?.name?.last} {data?.name?.first}
-        </div>
-    )
+  return (
+    <div>
+        Hello, {data?.name?.title} {data?.name?.last} {data?.name?.first}
+    </div>
+  )
 }
 
-export default csr
+export default Csr
